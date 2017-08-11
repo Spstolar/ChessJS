18588d5e3ab1833d7132c52072ae73c68f13d6ed
Spstolar <spstolar@math.uh.edu>
2017-08-11 14:17:11 -0500

    Adds beginning definitions for the board and pieces.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-d
+$(function() {  // this tests that JQuery is running
+	init();
+	console.log("Main Init Called");
+});
+
+function init() {
+	console.log("init() called");  // logging called functions to the browser is useful to see what is running and when 
+}
