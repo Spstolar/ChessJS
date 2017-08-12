767c5be6a47baafea49b145dc6da5f69ebaa1095
Spstolar <spstolar@math.uh.edu>
2017-08-12 00:02:38 -0500

    11: sets up the function that parses the FEN string and handles the first part about piece placement.

--- js/board.js
+++ js/board.js
@@ -85,6 +85,87 @@ function ResetBoard() {
 	GameBoard.moveListStart[GameBoard.Ply] = 0;
 }
 
+// Example FEN string:   rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3
+// rank8/rank7/.../rank1 to_move castling en_passant
 function ParseFen(fen) {
+	
 	ResetBoard();
+	
+	var rank = RANKS.RANK_8;
+	var file = FILES.FILE_A;
+	var piece = 0;
+	var count = 0;
+	var i = 0;
+	var sq120 = 0;
+	var fenCnt = 0;
+	
+	while ((rank >= RANKS.RANK_1) && fenCnt < fen.length) {
+		count = 1;
+		switch (fen[fenCnt]) {
+			case 'p': piece = PIECES.bP; break;
+			case 'r': piece = PIECES.bR; break;
+			case 'n': piece = PIECES.bN; break;
+			case 'b': piece = PIECES.bB; break;
+			case 'k': piece = PIECES.bK; break;
+			case 'q': piece = PIECES.bQ; break;
+			case 'P': piece = PIECES.wP; break;
+			case 'R': piece = PIECES.wR; break;
+			case 'N': piece = PIECES.wN; break;
+			case 'B': piece = PIECES.wB; break;
+			case 'K': piece = PIECES.wK; break;
+			case 'Q': piece = PIECES.wQ; break;
+			
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+				piece = PIECES.EMPTY;
+				count = fen[fenCnt].charCodeAt() - '0'.charCodeAt();
+				break;
+				
+			case '/':
+			case ' ':
+				rank--;
+				file = FILES.FILE_A;
+				fenCnt++;
+				continue;
+			default:
+				console.log("FEN error");
+				return;
+		}
+		
+		for (i = 0; i < count; i++) {
+			sq120 = ER2SQ(file,rank);
+			brd_pieces[sq120] = piece;
+			file++;
+		}
+		fenCnt++ 
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
